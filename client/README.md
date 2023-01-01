94c4fb669729ea3eedff089450b4770130c7176a
minh.tranvan <minh.tranvan@hdwebsoft.io>
2023-01-02 01:47:03 +0700

    Initial commit

---
+++ client/README.md
@@ -0,0 +1,21 @@
+# flutter_ecommerce
+
+
+
+A full-stack e-commerce mobile application, with a complete shopping cart for authenticated users.
+
+Complete app with an eye-catching UI using :  
+Flutter Material Library  
+Redux for global state management  
+Redux Thunk for async actions  
+Complete API with custom controllers with the help of Strapi  
+App data stored in a sqlite database  
+User Authentication with Register / Login.
+
+![register](./assets/register.jpg)
+![products](./assets/products.png)
+![productdetail](./assets/productdetail.png)
+![creditcards](./assets/creditcards.png)
+![checkout](./assets/checkout.png)
+![success](./assets/success.png)
+![orders](./assets/orders.png)
---
+++ client/lib/redux/actions.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+import 'package:flutter_ecommerce/models/product.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/models/user.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/* User Actions */
+ThunkAction<AppState> getUserAction = (Store<AppState> store) async {
+  final prefs = await SharedPreferences.getInstance();
+  final String storedUser = prefs.getString('user');
+  final user =
+      storedUser != null ? User.fromJson(json.decode(storedUser)) : null;
+  store.dispatch(GetUserAction(user));
+};
+
+ThunkAction<AppState> logoutUserAction = (Store<AppState> store) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove('user');
+  User user;
+  store.dispatch(LogoutUserAction(user));
+};
+
+class GetUserAction {
+  final User _user;
+
+  User get user => this._user;
+
+  GetUserAction(this._user);
+}
+
+class LogoutUserAction {
+  final User _user;
+
+  User get user => this._user;
+
+  LogoutUserAction(this._user);
+}
+
+/* Products Actions */
+ThunkAction<AppState> getProductsAction = (Store<AppState> store) async {
+  http.Response response = await http.get('http://localhost:1337/products');
+  final List<dynamic> responseData = json.decode(response.body);
+  List<Product> products = [];
+  responseData.forEach((productData) {
+    final Product product = Product.fromJson(productData);
+    products.add(product);
+  });
+  store.dispatch(GetProductsAction(products));
+};
+
+class GetProductsAction {
+  final List<Product> _products;
+
+  List<Product> get products => this._products;
+
+  GetProductsAction(this._products);
+}
+
+/* Cart Products Actions */
+ThunkAction<AppState> toggleCartProductAction(Product cartProduct) {
+  return (Store<AppState> store) {
+    final List<Product> cartProducts = store.state.cartProducts;
+    final int index =
+        cartProducts.indexWhere((product) => product.id == cartProduct.id);
+    bool isInCart = index > -1 == true;
+    List<Product> updatedCartProducts = List.from(cartProducts);
+    if (isInCart) {
+      updatedCartProducts.removeAt(index);
+    } else {
+      updatedCartProducts.add(cartProduct);
+    }
+    store.dispatch(ToggleCartProductAction(updatedCartProducts));
+  };
+}
+
+class ToggleCartProductAction {
+  final List<Product> _cartProducts;
+
+  List<Product> get cartProducts => this._cartProducts;
+
+  ToggleCartProductAction(this._cartProducts);
+}
